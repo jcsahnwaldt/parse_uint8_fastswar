8c992e1016c500b84b391e8d2a0770a852d1a863
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2023-12-08 19:41:06 +0100

    parse_uint8_fastswar.rs: first steps

--- rs/parse_uint8_fastswar.rs
+++ rs/parse_uint8_fastswar.rs
@@ -1,2 +1,38 @@
+use std::ffi::c_void;
+use std::ptr::addr_of;
+use std::mem::size_of;
+
+// according to godbolt.org, all of these functions generate
+// mov eax, dword ptr [rdi]
+
+fn foo(b: &[u8]) -> u32 {
+    let p = b.as_ptr() as *const u32;
+    unsafe { p.read_unaligned() }
+}
+
+extern "C" {
+    fn memcpy(dst: *mut c_void, src: *const c_void, count: usize) -> *mut c_void;
+}
+
+fn bar(b: &[u8]) -> u32 {
+    let i: u32 = 0;
+    unsafe {
+        memcpy(addr_of!(i) as *mut c_void, b.as_ptr() as *const c_void, size_of::<u32>());
+    };
+    return i;
+}
+
+fn baz(b: &[u8]) -> u32 {
+    let i: u32 = 0;
+    unsafe {
+        std::ptr::copy_nonoverlapping(b.as_ptr(), std::ptr::addr_of!(i) as *mut u8, size_of::<u32>());
+    }
+    return i;
+}
+
 fn main() {
+    let b: [u8; 4] = [1,2,3,4];
+    println!("{:X?}", foo(&b));
+    println!("{:X?}", bar(&b));
+    println!("{:X?}", baz(&b));
 }
