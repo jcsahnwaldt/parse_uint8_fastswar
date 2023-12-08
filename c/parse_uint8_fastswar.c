e3b45acc727371deabf9c7f7492c8a08e14170d2
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2023-12-08 23:18:54 +0100

    parse_uint8_fastswar.c: cosmetics

--- c/parse_uint8_fastswar.c
+++ c/parse_uint8_fastswar.c
@@ -5,8 +5,6 @@
 #include <limits.h>
 #include <stdarg.h>
 
-typedef int parse_fn(const char*, size_t, uint8_t*);
-
 static int parse_uint8_fastswar(const char *str, size_t len, uint8_t *num) {
   if (len == 0 || len > 3) { return 0; }
   uint32_t digits;
@@ -14,11 +12,7 @@ static int parse_uint8_fastswar(const char *str, size_t len, uint8_t *num) {
   digits ^= 0x30303030;
   digits <<= ((4 - len) * 8);
   *num = (uint8_t)((0x640a01 * digits) >> 24);
-#if 1
   uint32_t all_digits = ((digits | (0x06060606 + digits)) & 0xF0F0F0F0) == 0;
-#else
-  uint32_t all_digits = ((0x06060606 + digits) & 0xF0F0F0F0) == 0;
-#endif
   return all_digits & ((__builtin_bswap32(digits) <= 0x020505));
 }
 
@@ -28,16 +22,14 @@ static int parse_uint8_fastswar_bob(const char *str, size_t len, uint8_t *num) {
   digits ^= 0x303030;
   digits <<= (len ^ 3) * 8;
   *num = (uint8_t)((0x640a01 * digits) >> 16);
-#if 1
-  uint32_t all_digits = (((digits + 0x767676) | digits) & 0x808080) == 0;
-#else
-  uint32_t all_digits = ((digits + 0x767676) & 0x808080) == 0;
-#endif
+  uint32_t all_digits = ((digits | (digits + 0x767676)) & 0x808080) == 0;
   return all_digits && ((len ^ 3) < 3) && __builtin_bswap32(digits) <= 0x020505ff;
 }
 
+typedef int fun_t(const char*, size_t, uint8_t*);
+
 typedef struct {
-  parse_fn* fn;
+  fun_t* fun;
   char str[4];
   size_t len;
   int want;
@@ -56,7 +48,7 @@ static void error(test_t* t, const char* fmt, ...) {
 static void check(test_t* t) {
   int want = t->want;
   uint8_t got;
-  int ok = t->fn(t->str, t->len, &got);
+  int ok = t->fun(t->str, t->len, &got);
   if (want < 0 || want > 255) {
     if (ok) error(t, "expected error, got %d", got);
   }
@@ -78,9 +70,9 @@ static void test_rec(test_t* t) {
   t->len--;
 }
 
-static void test(parse_fn* fn, const char* name) {
+static void test(fun_t* fun, const char* name) {
   printf("testing %s\n", name);
-  test_t t = {fn, {0}, 0, 0};
+  test_t t = {fun, {0}, 0, 0};
   test_rec(&t);
 }
 
